54889b770c31e5343d0321e50dc72188e46fbcd7
phanetjava <chhernphannet0001@gmail.com>
2025-04-24 22:36:41 +0700

    finish created table model,brand

--- src/main/java/com/piseth/java/school/phones_shope/entity/Model.java
+++ src/main/java/com/piseth/java/school/phones_shope/entity/Model.java
@@ -1,21 +1,29 @@
 package com.piseth.java.school.phones_shope.entity;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "Brands")
-public class Brand {
+@Table(name = "Model")
+public class Model {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "brand_id")
+	@Column(name = "model_Id")
 	private Integer id;
 	@Column(name = "brand_name")
 	private String name;
+	@ManyToOne
+	@JoinColumn(name = "brand_id")
+	private Brand brand;
+
 }
