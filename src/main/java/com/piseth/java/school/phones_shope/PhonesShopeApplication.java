4ede6bdf8a7dca9161c52869c4143cdc50ac934e
phanetjava <chhernphannet0001@gmail.com>
2025-04-24 20:53:22 +0700

    start project with git

---
+++ src/main/java/com/piseth/java/school/phones_shope/PhonesShopeApplication.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.phones_shope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonesShopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonesShopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piseth/java/school/phones_shope/PhonesShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.phones_shope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonesShopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
